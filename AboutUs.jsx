c7ece40510ee5c2582286cb63a82b6e87d32babb
Ericcodesinjava <ericmathew200@gmail.com>
2026-05-03 23:48:06 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <p>We provide beautiful plants for your home and garden.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
